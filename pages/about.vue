80503268da9e041b057f44a4da216344fdb5107a
Mark van Wyk <mark@flyh2.com>
2021-02-08 15:12:15 +0200

    basic framework

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex flex-row justify-center pt-32">
+        <div class="w-64">
+            This is a very awesome project that has been created as a platform for the new super Miggster and future super products. Watch this space to find out more about all the cool upcoming things we're going to enjoy here.
+            <nuxt-link to="/" class="block text-center font-thin text-blue-300 hover:text-blue-200 pt-4">Back</nuxt-link>
+        </div>
+    </div>
+</template>
