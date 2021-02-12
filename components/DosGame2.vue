fb957d5951a6838290ef8e583ee136d53f7be861
Mark van Wyk <mark@flyh2.com>
2021-02-12 11:48:48 +0200

    Digger working

--- components/DosGame2.vue
+++ components/DosGame2.vue
@@ -1,6 +1,46 @@
 <template>
-    <div class="flex w-full h-screen items-center justify-center bg-blue-700">
-        <canvas id="canvas" ref="canvas"></canvas>
+    <div id="screen" class="flex w-full h-screen items-center justify-center bg-black"><!-- v-touch:tap="fullScreen" -->
+        <div class="absolute h-screen flex w-full justify-center items-center z-10 bg-black" v-if="state !== 'playing'">
+            <div v-if="state==='loading'">Loading...</div>
+            
+            <div v-if="state==='loaded'" class="py-2 px-4 border border-white rounded-md bg-blue-900 z-20 pointer-events-auto cursor-pointer" @click="start">START GAME</div>
+        </div>
+        <div class="absolute h-screen flex w-full justify-center">
+            <div class="flex flex-grow justify-center items-end" v-if="touch">
+                <div class="relative w-32 h-32 er-no-touch">
+                    <img src="/images/dos-console/D-Pad.svg" class="absolute flex w-32 h-32"/>
+                    <div class="flex absolute w-32 h-32 flex-col z-10">
+                        <div class="flex flex-grow w-32 text-white">
+                            <div class="flex flex-grow"></div>
+                            <div id="up" class="flex flex-grow"></div>
+                            <div class="flex flex-grow"></div>
+                        </div>
+                        <div class="flex flex-grow w-32 text-white">
+                            <div id="left" class="flex flex-grow"></div>
+                            <div class="flex flex-grow"></div>
+                            <div id="right" ref="right" class="flex flex-grow"></div>
+                        </div>
+                        <div class="flex flex-grow w-32 text-white">
+                            <div class="flex flex-grow"></div>
+                            <div id="down" class="flex flex-grow"></div>
+                            <div class="flex flex-grow"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <canvas id="canvas" ref="canvas" class="flex"></canvas>
+            <div class="flex flex-grow flex-col justify-end items-center er-no-touch" v-if="touch">
+                <div  class="flex relative w-20 h-20">
+                    <img src="/images/dos-console/Btn-B.svg" class="absolute w-20 h-20"/>
+                    <div id="b" ref="b" class="absolute w-20 h-20 z-10"></div>
+                </div>
+                <div class="h-2"></div>
+                <div class="flex relative w-20 h-20">
+                    <img src="/images/dos-console/Btn-A.svg" class="absolute w-20 h-20"/>
+                    <div id="a" ref="a" class="absolute w-20 h-20 z-10"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,11 +49,37 @@ export default {
 
     // Include JavaScript
     // Type Cycles to Integer
+    // Reasonable Defaults
+    // Clean Up (Refactor for React)
+    // Splash Screen Overlay
+    // Make it available for multiple games
+    // Create keycode mapping
 
     name: "DosGame2",
     props: [
         'path', 'exec', 'cycles', 'opts'
     ],
+    data() {
+        return {
+            directionActive: 'none',
+            buttonActive: 'none',
+            touch: this.isTouch(),
+            state: 'loading',
+            directions: ['up', 'down', 'left', 'right'],
+            buttons: ['a', 'b'],
+            lastDirection: undefined,
+            lastButton: undefined,
+            keyCodeMapping: {
+                'up':'38',
+                'down':'40',
+                'left':'37',
+                'right':'39',
+                'a':'112',
+                'b':'32',
+            }
+
+        }
+    },
     mounted() {
         let canvas = this.$refs.canvas;
         let self = this;
@@ -26,17 +92,96 @@ export default {
                     (ci) => {
                         window.Module = ci.dos;
                         window.ci = ci;
+                        self.state = 'loaded'
                     }
                 )
             )
-        })
+        });
+    },
+    methods: {
+        start() {
+            console.log("start")
+            this.fullScreen();
+            this.state = 'playing';
+            this.setupListeners();
+        },
+        fullScreen() {
+            if (!document.fullscreenElement) {
+                document.documentElement.requestFullscreen();
+            }
+        },
+        handleTouchEvent(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            let direction = undefined
+            let button = undefined
+
+            for (let i = 0; i < event.touches.length; i++) {
+                let x = event.touches[i].clientX;
+                let y = event.touches[i].clientY;
+                let element = document.elementFromPoint(x, y);
+                if (element.id) {
+                    if (this.directions.includes(element.id)) {
+                        direction = element.id;
+                    } else if (this.buttons.includes(element.id)) {
+                        button = element.id;
+                    }
+                }
+            }
+            if (this.lastDirection && direction !== this.lastDirection) {
+                this.simulateKeyPress(this.lastDirection, false);
+            }
+            if (direction) {
+                this.simulateKeyPress(direction, true)
+            }
+            if (this.lastButton && button !== this.lastButton) {
+                this.simulateKeyPress(this.lastButton, false);
+            }
+            if (button) {
+                this.simulateKeyPress(button, true)
+            }
+            this.lastDirection = direction;
+            this.lastButton = button;
+        },
+        simulateKeyPress(action, state) {
+            console.log (action + " " + (state ? 'on':'off'));
+            if (window.ci) {
+
+                window.ci.simulateKeyEvent(this.keyCodeMapping[action], state);
+            }
+        },
+        isTouch() {
+            return (('ontouchstart' in window) ||
+                (navigator.maxTouchPoints > 0) ||
+                (navigator.msMaxTouchPoints > 0));
+
+        },
+        setupListeners() {
+            try {
+                document.removeEventListener('touchmove', this.handleTouchEvent)
+                document.removeEventListener('touchstart', this.handleTouchEvent)
+                document.removeEventListener('touchend', this.handleTouchEvent)
+            } finally {
+                document.addEventListener('touchmove', this.handleTouchEvent)
+                document.addEventListener('touchstart', this.handleTouchEvent)
+                document.addEventListener('touchend', this.handleTouchEvent)
+            }
+        }
     }
 }
 </script>
 
 <style scoped>
-    canvas {
+    #screen{
         user-select: none;
+    }
+
+    canvas {
         height: 80vh;
     }
+
+    .er-no-touch {
+        touch-action: none;
+    }
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-    <section class="pt-48">
+    <section class="flex flex-col h-screen justify-center items-center">
         <div class="text-center font-bold text-4xl">ARCADE-X</div>
         <div class="text-center font-thin text-2xl">THE REBUILD</div>
-        <nuxt-link to="about" class="block pt-4 block text-center font-thin text-blue-300 hover:text-blue-200">Find out more</nuxt-link>
-        <nuxt-link to="/games/dos/digger" class="block pt-4 block text-center font-thin text-blue-300 hover:text-blue-200">Play Digger</nuxt-link>
+        <nuxt-link to="about" class="block pt-2 block text-center font-thin text-blue-300 hover:text-blue-200">Find out more</nuxt-link>
+        <nuxt-link to="/games/dos/digger" class="block pt-2 block text-center font-thin text-blue-300 hover:text-blue-200">Play Digger</nuxt-link>
     </section>
 </template>
