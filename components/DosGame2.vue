8ee83f9258f584688fccacfa7855a0157befd6de
Mark van Wyk <mark@flyh2.com>
2021-02-12 16:37:08 +0200

    Full Screen and Orientation working on Firefox Simulated

--- components/DosGame2.vue
+++ components/DosGame2.vue
@@ -1,26 +1,39 @@
 <template>
-    <div id="screen" class="flex w-full h-screen items-center justify-center bg-black"><!-- v-touch:tap="fullScreen" -->
-        <div class="absolute h-screen flex w-full justify-center items-center z-10 bg-black" v-if="state !== 'playing'">
+    <div id="screen" ref="screen" class="flex w-full h-screen items-center justify-center bg-black">
+        <nuxt-link to="/" class="absolute top-0 right-0 m-3 z-40 p-1 rounded-full border-4 border-gray-500">
+            <svg class="text-white w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </nuxt-link>
+        <div class="absolute h-screen flex flex-col w-full justify-center items-center z-30 bg-black" v-if="!landscape">
+            <div class="py-2 px-4 text-white pointer-events-auto cursor-pointer flex flex-col items-center">
+                <svg class="w-32 h-32" stroke="none" fill="currentColor" enable-background="new 0 0 100 100"  version="1.1" viewBox="0 0 100 100" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><switch><foreignObject width="1" height="1" requiredExtensions="http://ns.adobe.com/AdobeIllustrator/10.0/"/><g><path d="m5273.1 2400.1v-2c0-2.8-5-4-9.7-4s-9.7 1.3-9.7 4v2c0 1.8 0.7 3.6 2 4.9l5 4.9c0.3 0.3 0.4 0.6 0.4 1v6.4c0 0.4 0.2 0.7 0.6 0.8l2.9 0.9c0.5 0.1 1-0.2 1-0.8v-7.2c0-0.4 0.2-0.7 0.4-1l5.1-5c1.3-1.3 2-3.1 2-4.9zm-9.7-0.1c-4.8 0-7.4-1.3-7.5-1.8 0.1-0.5 2.7-1.8 7.5-1.8s7.3 1.3 7.5 1.8c-0.2 0.5-2.7 1.8-7.5 1.8z"/><path d="m5268.4 2410.3c-0.6 0-1 0.4-1 1s0.4 1 1 1h4.3c0.6 0 1-0.4 1-1s-0.4-1-1-1h-4.3z"/><path d="m5272.7 2413.7h-4.3c-0.6 0-1 0.4-1 1s0.4 1 1 1h4.3c0.6 0 1-0.4 1-1s-0.4-1-1-1z"/><path d="m5272.7 2417h-4.3c-0.6 0-1 0.4-1 1s0.4 1 1 1h4.3c0.6 0 1-0.4 1-1 0-0.5-0.4-1-1-1z"/><path d="m8 21.2h31.3v20.3h5.5v-27.4c0-2.4-1.9-4.2-4.2-4.2h-33.9c-2.4 0-4.2 1.9-4.2 4.2v65.9c0 2.4 1.9 4.2 4.2 4.2h10.1v-16.3h-8.8v-46.7zm10.7-7.1h9.7c0.8 0 1.4 0.6 1.4 1.4s-0.6 1.4-1.4 1.4h-9.7c-0.8 0-1.4-0.6-1.4-1.4s0.7-1.4 1.4-1.4z"/><path d="m93.2 47.8h-65.8c-2.4 0-4.3 1.9-4.3 4.3v33.8c0 2.4 1.9 4.3 4.3 4.3h65.8c2.3 0 4.3-1.9 4.3-4.3v-33.8c0-2.3-1.9-4.3-4.3-4.3zm-61.9 24.8c-2 0-3.5-1.6-3.5-3.5 0-2 1.6-3.6 3.5-3.6 2 0 3.5 1.6 3.5 3.6 0 1.9-1.6 3.5-3.5 3.5zm54.8 12h-46.7v-31.3h46.7v31.3zm7.1-10.8c0 0.8-0.6 1.4-1.4 1.4s-1.4-0.6-1.4-1.4v-9.7c0-0.8 0.6-1.4 1.4-1.4s1.4 0.6 1.4 1.4v9.7z"/><path d="m62.2 33.8c-0.5-0.5-0.7-1.1-0.7-1.7s0.2-1.3 0.7-1.7c1-1 2.5-1 3.5 0l2.1 2.1c-0.7-6.5-6.2-11.7-12.9-11.7-1.4 0-2.5-1.1-2.5-2.5s1.1-2.5 2.5-2.5c9.5 0 17.3 7.5 17.9 16.8l2.4-2.4c1-1 2.5-1 3.5 0s1 2.5 0 3.5l-6.5 6.5c-1 1-2.5 1-3.5 0l-6.5-6.4z"/></g></switch></svg>
+                <span>rotate device</span>
+            </div>
+        </div>
+        <div class="absolute h-screen flex flex-col w-full justify-center items-center z-20 bg-black" v-if="!fullscreen">
+            <div class="py-2 px-4 border border-white rounded-md bg-blue-900 pointer-events-auto cursor-pointer" @click="goFullScreen()">ENTER FULLSCREEN</div>
+        </div>
+        <div class="absolute h-screen flex flex-col w-full justify-center items-center z-10 bg-black" v-if="state !== 'playing'">
             <div v-if="state==='loading'">Loading...</div>
-            
-            <div v-if="state==='loaded'" class="py-2 px-4 border border-white rounded-md bg-blue-900 z-20 pointer-events-auto cursor-pointer" @click="start">START GAME</div>
+            <div v-if="state==='loaded'" class="py-2 px-4 border border-white rounded-md bg-blue-900 z-10 pointer-events-auto cursor-pointer" @click="startGame">START GAME</div>
         </div>
         <div class="absolute h-screen flex w-full justify-center">
             <div class="flex flex-grow justify-center items-end" v-if="touch">
-                <div class="relative w-32 h-32 er-no-touch">
-                    <img src="/images/dos-console/D-Pad.svg" class="absolute flex w-32 h-32"/>
-                    <div class="flex absolute w-32 h-32 flex-col z-10">
-                        <div class="flex flex-grow w-32 text-white">
+                <div class="relative w-40 h-40 er-no-touch mb-10">
+                    <img src="/images/dos-console/D-Pad.svg" class="absolute flex w-40 h-40"/>
+                    <div class="flex absolute w-40 h-40 flex-col z-10">
+                        <div class="flex flex-grow w-40">
                             <div class="flex flex-grow"></div>
                             <div id="up" class="flex flex-grow"></div>
                             <div class="flex flex-grow"></div>
                         </div>
-                        <div class="flex flex-grow w-32 text-white">
+                        <div class="flex flex-grow w-40">
                             <div id="left" class="flex flex-grow"></div>
                             <div class="flex flex-grow"></div>
                             <div id="right" ref="right" class="flex flex-grow"></div>
                         </div>
-                        <div class="flex flex-grow w-32 text-white">
+                        <div class="flex flex-grow w-40">
                             <div class="flex flex-grow"></div>
                             <div id="down" class="flex flex-grow"></div>
                             <div class="flex flex-grow"></div>
@@ -35,7 +48,7 @@
                     <div id="b" ref="b" class="absolute w-20 h-20 z-10"></div>
                 </div>
                 <div class="h-2"></div>
-                <div class="flex relative w-20 h-20">
+                <div class="flex relative w-20 h-20 mb-10">
                     <img src="/images/dos-console/Btn-A.svg" class="absolute w-20 h-20"/>
                     <div id="a" ref="a" class="absolute w-20 h-20 z-10"></div>
                 </div>
@@ -47,6 +60,9 @@
 <script>
 export default {
 
+    // Don't allow screen dragging
+    // Detect fullscreen,
+
     // Include JavaScript
     // Type Cycles to Integer
     // Reasonable Defaults
@@ -54,6 +70,7 @@ export default {
     // Splash Screen Overlay
     // Make it available for multiple games
     // Create keycode mapping
+    // Title
 
     name: "DosGame2",
     props: [
@@ -69,6 +86,8 @@ export default {
             buttons: ['a', 'b'],
             lastDirection: undefined,
             lastButton: undefined,
+            landscape: undefined,
+            fullscreen: undefined,
             keyCodeMapping: {
                 'up':'38',
                 'down':'40',
@@ -81,6 +100,10 @@ export default {
         }
     },
     mounted() {
+        this.checkOrientation();
+        this.checkFullscreen();
+        this.setupScreenListeners();
+
         let canvas = this.$refs.canvas;
         let self = this;
         Dos(canvas, {
@@ -99,16 +122,12 @@ export default {
         });
     },
     methods: {
-        start() {
-            console.log("start")
-            this.fullScreen();
+        startGame() {
+            this.setupTouchListeners();
             this.state = 'playing';
-            this.setupListeners();
         },
-        fullScreen() {
-            if (!document.fullscreenElement) {
-                document.documentElement.requestFullscreen();
-            }
+        goFullScreen() {
+            this.$refs.screen.requestFullscreen();
         },
         handleTouchEvent(event) {
             event.preventDefault();
@@ -146,7 +165,6 @@ export default {
         simulateKeyPress(action, state) {
             console.log (action + " " + (state ? 'on':'off'));
             if (window.ci) {
-
                 window.ci.simulateKeyEvent(this.keyCodeMapping[action], state);
             }
         },
@@ -156,16 +174,35 @@ export default {
                 (navigator.msMaxTouchPoints > 0));
 
         },
-        setupListeners() {
+        setupTouchListeners() {
+            console.log ("setting up")
             try {
-                document.removeEventListener('touchmove', this.handleTouchEvent)
-                document.removeEventListener('touchstart', this.handleTouchEvent)
-                document.removeEventListener('touchend', this.handleTouchEvent)
+                this.$refs.screen.removeEventListener('touchmove', this.handleTouchEvent)
+                this.$refs.screen.removeEventListener('touchstart', this.handleTouchEvent)
+                this.$refs.screen.removeEventListener('touchend', this.handleTouchEvent)
             } finally {
-                document.addEventListener('touchmove', this.handleTouchEvent)
-                document.addEventListener('touchstart', this.handleTouchEvent)
-                document.addEventListener('touchend', this.handleTouchEvent)
+                this.$refs.screen.addEventListener('touchmove', this.handleTouchEvent)
+                this.$refs.screen.addEventListener('touchstart', this.handleTouchEvent)
+                this.$refs.screen.addEventListener('touchend', this.handleTouchEvent)
             }
+        },
+        setupScreenListeners() {
+            try {
+            } finally {
+                self = this;
+                window.addEventListener("orientationchange", this.checkOrientation);
+                document.addEventListener("fullscreenchange", this.checkFullscreen);
+            }
+        },
+        checkOrientation() {
+            setTimeout(() => {
+                this.landscape = window.screen.width > window.screen.height;
+            })
+        },
+        checkFullscreen() {
+            setTimeout(() => {
+                this.fullscreen = !!document.fullscreenElement;
+            })
         }
     }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
     <section class="flex flex-col h-screen justify-center items-center">
         <div class="text-center font-bold text-4xl">ARCADE-X</div>
         <div class="text-center font-thin text-2xl">THE REBUILD</div>
-        <nuxt-link to="about" class="block pt-2 block text-center font-thin text-blue-300 hover:text-blue-200">Find out more</nuxt-link>
-        <nuxt-link to="/games/dos/digger" class="block pt-2 block text-center font-thin text-blue-300 hover:text-blue-200">Play Digger</nuxt-link>
+        <nuxt-link to="/games/dos/digger" class="mt-4 py-2 px-4 border border-white rounded-md bg-blue-900 z-20 pointer-events-auto cursor-pointer">PLAY DIGGER</nuxt-link>
     </section>
 </template>
