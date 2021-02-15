c1f1c1a72e6cbdd3ba6d21b5c96797628f11ec00
Mark van Wyk <mark@flyh2.com>
2021-02-15 07:44:11 +0200

    changed speed

--- pages/games/dos/_game.vue
+++ pages/games/dos/_game.vue
@@ -12,7 +12,7 @@ export default {
     data() {
         return {
             games: {
-                "digger": {         title:'Digger',         path: '/games/dos/digger.zip',        exec: 'DIGGER.COM',                   opts: '-c',     cycles:750},
+                "digger": {         title:'Digger',         path: '/games/dos/digger.zip',        exec: 'DIGGER.COM',                   opts: '-c',     cycles:400},
                 "bubble-bobble": {  title:'Bubble Bobble',  path: '/games/dos/bubble_bobble.zip', exec: './Bubble_Bobble/BUBBLE.EXE',   opts: '',       cycles:750}
             }
         }
