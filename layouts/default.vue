4e4637430223e51bf03b0f8770daca42540f67ed
Mark van Wyk <mark@flyh2.com>
2021-02-19 10:56:22 +0200

    background black

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="h-screen bg-gray-900 text-white">
+    <section class="h-screen bg-black text-white">
         <Nuxt />
         <!--
         <div class="absolute bottom-0 w-full text-sm p-2 text-right">&copy; Emerge Gaming &mdash; {{year}}</div>
